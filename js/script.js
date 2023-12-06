04b29bd667553e42a8fe02d99c445180de255a00
zGiusee <giusebelforte@gmail.com>
2023-12-06 16:01:42 +0100

    [UDPATE] snack 1 done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-// SNACK 2
+//****************************
+//     INIZIO SNACK 2
+//****************************
+
+// FUNZIONI
 
 // Creo la funzione che generi numeri randomici
 function rng(){
@@ -6,6 +10,8 @@ function rng(){
 
     return rngNumber;
 }
+
+// FINE FUNZIONI
 
 // Creo l'array di squadre di calcio con i rispettivi valori
 const teams = [
@@ -37,8 +43,6 @@ const teams = [
     },
 ];
 
-console.log(teams)
-
 // Ciclo l'array per dare un valore agli elementi usando la funzione dei numeri randomici
 teams.forEach((elem) =>{
     
@@ -56,21 +60,102 @@ let namesAndFouls = [ ];
 // Ciclo l'array e prendo solo le info che mi servono per poi pusharli dentro l'array precedentemente definito
 teams.forEach((elem) =>{
 
-    // VERSIONE CON L'ARRAY
     let { name, falliSubiti } = elem;
 
-    namesAndFouls.push(name);
-    namesAndFouls.push(falliSubiti);
+    // VERSIONE CON L'ARRAY
+    // namesAndFouls.push(name);
+    // namesAndFouls.push(falliSubiti);
 
     // VERSIONE CON GLI OGGETTI
-    // let obj = {
-    //     name,
-    //     falliSubiti,
-    // }
+    let obj = {
+        name,
+        falliSubiti,
+    }
 
-    // namesAndFouls.push(obj);
+    namesAndFouls.push(obj);
 });
 
 // Controllo che sia stato eseguito tutto correttamente
 console.log(namesAndFouls)
 
+
+//****************************
+//     FINE SNACK 2
+//****************************
+
+
+
+//****************************
+//     INIZIO SNACK 1
+//****************************
+
+
+// Creo l'array delle bici
+
+const bicycles = [
+
+    {
+        bicycleName : 'Colnago',
+        weight : 5.3
+    },
+    {
+        bicycleName : 'Van Rysel',
+        weight : 10.2
+    },
+    {
+        bicycleName : 'Triban',
+        weight : 3.6
+    },
+    {
+        bicycleName : 'Cannondale',
+        weight : 7
+    },
+    {
+        bicycleName : 'Trek',
+        weight : 4.3
+    },
+];
+
+// Faccio in modo di creare un confronto tra la variabile lighterBicycle e una delle mie bici in questo caso la 0
+// che poi andro a ciclare per farla confrontare con tutte le altre
+let lighterBicycle = bicycles[0].weight;
+
+bicycles.forEach((elem) => {
+
+    if(elem.weight < lighterBicycle){
+        lighterBicycle = elem.weight;
+    }
+    
+    console.log(lighterBicycle)
+
+});
+
+// Eseguo un altro ciclo usando la destrutturazione stampandolo in console
+bicycles.forEach((elem) => {
+
+    if(lighterBicycle == elem.weight){
+        let { bicycleName, weight} = elem;
+        console.log(`la bici col peso minore è ${bicycleName} con un peso di ${weight}kg`)
+    }
+
+});
+
+
+// for(let i = 0; i < bicycles.length; i++){
+
+//     if(bicycles[i].weight < lighterBicycle){
+
+//         lighterBicycle = bicycles[i].weight;
+
+//         console.log(lighterBicycle)
+//     }
+
+//     // let { bicycleName, weight} = 
+
+// }
+// // console.log(lighterBicycle)
+
+
+//****************************
+//     FINE SNACK 1
+//****************************
